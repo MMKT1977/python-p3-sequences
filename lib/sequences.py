e7c9db86c946b128bf276f6fa34c6caaf2af970f
MMKT1977 <kennedy.moranga@student.moringaschool..com>
2025-03-24 13:55:44 +0300

    fibonacci function implemented

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = []
+    if length >= 1:
+        fibonacci.append(0)
+    if length >=2:
+        fibonacci.append(1)
+    for i in range (2,length):
+        next_number = fibonacci[-1]+ fibonacci[-2]
+        fibonacci.append(next_number)
+    print(fibonacci)
+print_fibonacci(11)
+
+    
